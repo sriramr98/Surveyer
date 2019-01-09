f9bf159c451b5a3ade46c3a7534c6d8c3da26a4a
Sriram R <sriramr083@gmail.com>
2019-01-09 12:29:15 +0530

    added basic passport configuration for google oauth route

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,3 +1,5 @@
 module.exports = {
-    PORT: process.env.PORT || 3000
+    PORT: process.env.PORT || 3000,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID
 };
--- server/server.js
+++ server/server.js
@@ -1,7 +1,26 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 require('dotenv').config();
 
+const keys = require('./config/keys');
+
 const app = express();
+
+// setup passport
+passport.use(
+    new GoogleStrategy(
+        {
+            clientID: keys.GOOGLE_CLIENT_ID,
+            clientSecret: keys.GOOGLE_CLIENT_SECRET,
+            callbackURL: '/api/auth/google/callback'
+        },
+        accessToken => {
+            console.log(accessToken);
+        }
+    )
+);
 
 app.get('/', (req, res) => {
     res.send('Welcome to surveyer');
